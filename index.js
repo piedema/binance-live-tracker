b7b93dd31eb63a6548094afc5e13c1921c0c333f
piedema <60652568+piedema@users.noreply.github.com>
2020-06-19 14:05:51 +0200

    added keys const

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const blt = require('./binance-live-tracker')()
-const binanceKeys = require('../keys.json')
+
+const binanceKeys = {
+  APIKEY:'your binance api key',
+  APISECRET:'your binance api secret'
+}
 
 const options = {
   binance:binanceKeys,
@@ -9,7 +13,6 @@ const options = {
 }
 
 blt.setOptions(options)
-
 blt.init()
 
 blt.events.on('spot.myOrders', data => console.log(data))
